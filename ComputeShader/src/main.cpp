8031b563bc073e40b7c2c0ab0c520ac33007f270
吉井 大輝 <0214.taiki@gmail.com>
2017-08-30 19:45:06 +0900

    ｓｓｓ

--- ComputeShader/src/main.cpp
+++ ComputeShader/src/main.cpp
@@ -6,11 +6,14 @@
 std::string DIR;
 std::string FILENAME;
 
+// GPU側でのループ回数
 #define num_loop_x 10
 #define num_loop_y 10
+// GPU側に何個スレッドを用意するか(多いほうが早い)
 #define num_sled_x 10
 #define num_sled_y 10
 
+// GPU側と共有する構造体
 struct DATA
 {
 	float arr[num_loop_x][num_loop_y];
@@ -18,40 +21,52 @@ struct DATA
 
 void main()
 {
+	// カレントディレクトリ取得
 	char DIR_[255];
 	GetCurrentDirectory(255, DIR_);
 	DIR = std::string(DIR_) + "\\src\\";
-	FILENAME = "p2.gpu";
+	FILENAME = "program.gpu";
 
+	// gpgpu初期化
 	gpgpu::initGL();
-	if (gpgpu::error != "") {
+	if (gpgpu::error != "") // エラー確認
+	{
 		std::cout << gpgpu::error << std::endl;
 		std::getchar();
 		return;
 	}
 
+	// gpu側に変数を追加
 	gpgpu::var<DATA> data;
 
+	// 変数初期化
 	for (int i = 0; i < num_loop_x * num_loop_y; i++) {
 		data.data->arr[i % num_loop_x][i / num_loop_x] = 0.0f;
 	}
 
+	// gpu側に関数を追加
 	gpgpu::func func(DIR + FILENAME);
+
+	// 何回ループするかを指定
 	func.set_loop(num_loop_x, num_sled_x, num_loop_y, num_sled_y);
+
+	// gpu側に変数を送信
+	data.set();
+
+	// 変数を渡して計算
+	func.Compute(data);
+	if (func.checkError()) // エラー確認
 	{
-		data.set();
-		func.Compute(data);
-		if (func.checkError())
-		{
-			std::cout << func.getCode() << std::endl;
-			std::cout << "エラー\n" << func.getError() << std::endl;
-			std::getchar();
-			return;
-		}
-		data.get();
+		std::cout << func.getCode() << std::endl;
+		std::cout << "エラー\n" << func.getError() << std::endl;
+		std::getchar();
+		return;
 	}
 
-	
+	// gpu側の変数を取得
+	data.get();
+
+	// 計算結果表示
 	for (int j = 0; j < 10; j++) {
 		for (int i = 0; i < 10; i++) {
 			std::cout << "[";
@@ -65,7 +80,10 @@ void main()
 		else std::cout << "\n";
 	}
 
-	std::getchar();
+	// 終了処理
 	gpgpu::exitGL();
+
+	// キー入力があるまで待機
+	std::getchar();
 	return;
 }
